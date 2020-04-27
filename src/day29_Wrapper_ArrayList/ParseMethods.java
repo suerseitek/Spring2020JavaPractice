a95a5deb0e52ae0f07019ffb6115f877da655677
Seitek Suerkulov <suerseitek@gmail.com>
2020-04-27 11:51:45 -0400

    Day29 Class

--- src/day29_Wrapper_ArrayList/ParseMethods.java
+++ src/day29_Wrapper_ArrayList/ParseMethods.java
@@ -24,12 +24,54 @@ public class ParseMethods {
         // Integer  = (int)byte;
         // Integer = int;
 
+        String str2 = "12.5";
+
+        float f1 =  Float.parseFloat( str2 );   // float 12.5
+
+        System.out.println(f1 + 1);  // 13.5
+
+        double f2 =  Double.parseDouble(str2);   // double 12.5
+
+        System.out.println( f2 + 1 );  // 13.5
+
+
+        String str3 = "3147483647";
+
+        long num1 = Long.parseLong(str3);   //long 3147483647L
+
+        System.out.println(num1 + 2);
+
+        Long num2 = Long.parseLong(str3);  // autoboxing
+
+        System.out.println(num2 + 2);
+
+
+        String result1 = "TrUE"; //  not "true" or not "false" ==> false
+
+        boolean r1 =Boolean.parseBoolean(result1);   // boolean, false
+
+        System.out.println( r1 );
+
+
+        String result2 = "fALsE"; // "false"
+
+        boolean r2 = Boolean.parseBoolean(result2);
+
+        System.out.println(r2);
+
+
+
+
+
+
+
+
+
 
 
 
 
     }
-
 
 
 
