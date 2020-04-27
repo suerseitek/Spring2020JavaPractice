17baa24c0a097b28d41f136bff3705348840d8c2
Seitek Suerkulov <suerseitek@gmail.com>
2020-04-27 10:23:52 -0400

    day29Class

---
+++ src/day29_Wrapper_ArrayList/WrapperClass.java
@@ -0,0 +1,19 @@
+package day29_Wrapper_ArrayList;
+
+public class WrapperClass {
+
+    public static void main(String[] args) {
+
+        int a =100; //primitive
+
+        Integer b =200;
+        short s1 = 100;
+        int n1 = s1;
+        Integer c = n1;
+
+
+
+    }
+
+
+}
