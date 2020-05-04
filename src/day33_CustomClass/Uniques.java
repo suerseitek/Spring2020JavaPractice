ec20b1481d4b4235884ad760f369bac591e7e873
Seitek Suerkulov <suerseitek@gmail.com>
2020-05-04 10:21:06 -0400

    Day33 Class

---
+++ src/day33_CustomClass/Uniques.java
@@ -0,0 +1,65 @@
+package day33_CustomClass;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Uniques {
+
+    /*
+    1. write a program that can return the unique objects from a anArray List of Integers
+			Ex:
+				list: {1,1,2,3,4,5,5}
+				sout(list); ==> {2,3,4};
+		Note: 1. DO NOT use any extra arrayList
+			  2. DO NOT use any loops
+			  3. do not use any sort method
+			  4. use predicate only and collections methods only
+		Hint:   Collections.frequency() // will return the frequency
+				removeIf( frequency == 1)  can keep the unique objects in arrayList
+     */
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,1,2,3,4,5,5));
+
+
+       list.removeIf(p ->Collections.frequency(list, p) >1);
+
+        System.out.println(list);
+
+
+        System.out.println("====================================================");
+
+
+        /*
+        2. write a program that can return the duplicated objects from a an ArrayList of Integers
+			Ex:
+				list: {1,1,2,3,4,5,5}
+				sout(list); ==> {1,1,5,5};
+		Note: 1. DO NOT use any extra arrayList
+			  2. DO NOT use any loops
+			  3. DO NOT use any sort method
+			  4. Use predicate and collections methods only
+		Hint:   Collections.frequency() // will return the frequency
+				removeIf( frequency > 1)  can keep the duplicated objects in arrayList
+         */
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        list1.addAll(Arrays.asList(1,1,2,3,4,5,5));
+
+        list1.removeIf(x ->Collections.frequency(list1, x) <= 1);
+
+        System.out.println(list1);
+
+
+
+
+    }
+
+
+
+
+}
