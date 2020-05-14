33a59b4c6b3c02b0b37e98009447b012a464600f
Seitek Suerkulov <suerseitek@gmail.com>
2020-05-14 14:10:55 -0400

    Day39 Class

---
+++ src/day39_AccessModifiers/AccessModifiers.java
@@ -0,0 +1,15 @@
+package day39_AccessModifiers;
+
+
+public class AccessModifiers {
+
+
+
+    int defaultAccess = 1000; //default: open to the classes in same package with
+
+    public int publicAccess = 2000; //open to the world
+
+
+    private long SSN = 1234567;
+
+}
